505552ffdd4fc59e817d5bd856c35e58a26f6498
shyyko.serhiy@gmail.com <shyyko.serhiy@gmail.com>
2016-09-08 11:47:43 +0300

    added tests for play()

--- test/test.ts
+++ test/test.ts
@@ -56,3 +56,66 @@ describe('#pause()', function () {
         }).catch(done);
     });
 });
+
+describe('#play()', function () {
+    this.timeout(10000);
+    it('should fail if not initialized', function (done) {
+        const spotilocal = new Spotilocal();
+        spotilocal.play('spotify:track:3cANM3NuUjRDTi8fdU8P6q', 'spotify:user:shyyko.serhiy:playlist:4SdN0Re3tJg9uG08z2Gkr1').then(() => {
+            done('Should have failed');
+        }).catch((error) => {
+            assert.strictEqual(error, SPOTILOCAL_IS_NOT_INITIALIZED);
+            done();
+        }).catch((error) => {
+            done(error);
+        });
+    });
+
+    it('should play song wihtout context and return correct status', function (done) {
+        const spotilocal = new Spotilocal();
+        const trackUri = 'spotify:track:53zHrgxt8Xy1RkMWepJVUh';
+        spotilocal.init().then((spotilocal) => {
+            return spotilocal.play(trackUri)
+        }).then((status) => {
+            assert.strictEqual(status.playing, true);
+            assert.strictEqual(status.track.track_resource.uri, trackUri);
+            assert.strictEqual(status.track.track_resource.name, 'Insane');
+            done();
+        }).catch(done);
+    });
+
+    it('should play song in context and return correct status', function (done) {
+        const spotilocal = new Spotilocal();
+        const trackUri = 'spotify:track:3cANM3NuUjRDTi8fdU8P6q';
+        spotilocal.init().then((spotilocal) => {
+            return spotilocal.play(trackUri, 'spotify:user:shyyko.serhiy:playlist:4SdN0Re3tJg9uG08z2Gkr1')
+        }).then((status) => {
+            assert.strictEqual(status.playing, true);
+            assert.strictEqual(status.track.track_resource.uri, trackUri);
+            assert.strictEqual(status.track.track_resource.name, 'Brother');
+            done();
+        }).catch(done);
+    });
+});
+
+describe('#play() and #pause()', function () {
+    it('should play song pause it and resume it', function (done) {
+        const spotilocal = new Spotilocal();
+        const trackUri = 'spotify:track:3cANM3NuUjRDTi8fdU8P6q';
+        spotilocal.init().then((spotilocal) => {
+            return spotilocal.play(trackUri, 'spotify:user:shyyko.serhiy:playlist:4SdN0Re3tJg9uG08z2Gkr1')
+        }).then((status) => {
+            assert.strictEqual(status.playing, true);
+            assert.strictEqual(status.track.track_resource.uri, trackUri);
+            return spotilocal.pause(true);
+        }).then((status)=>{
+            assert.strictEqual(status.playing, false);
+            assert.strictEqual(status.track.track_resource.uri, trackUri);
+            return spotilocal.pause(false);            
+        }).then((status)=>{
+            assert.strictEqual(status.playing, true);
+            assert.strictEqual(status.track.track_resource.uri, trackUri);
+            done();         
+        }).catch(done);
+    });
+});
